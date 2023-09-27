a0cc7272bc05ea329b18b4d36c6acaf53b7e30d4
Felipe-ABS <FelipeABS2@Gmail.com>
2023-09-27 03:06:44 -0300

    28 - Validação de Formulário com Reactive Forms

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -8,9 +9,32 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MomentFormComponent implements OnInit {
   @Input() btnText!: string;
 
+  momentForm!: FormGroup; // Aqui o FormGroup é apenas declarado
+
   constructor() { }
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({ // Aqui o FormGroup é inicializado
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!; // atributo de title dentro do momentForm no FormGroup no ngOnInit, exclamação para indicar que os valores vão existir
+  }
+
+  get description() {
+    return this.momentForm.get('description')!; // Colocar a exclamação pra avisar ao angular que o valor deles vão existir
+  }
+
+  submit(): void {
+    if(this.momentForm.invalid) {
+      return;
+    }
+    console.log("Enviou Formulário");
   }
 
 }
