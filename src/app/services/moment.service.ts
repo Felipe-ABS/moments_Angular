f3e45fcb40f23724c6dc41697c1aa4f27fb01e35
Felipe-ABS <FelipeABS2@Gmail.com>
2023-09-28 18:20:34 -0300

    32 - Resgatando dados do banco

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 
 import { Moment } from '../Moment';
 
+import { Response } from '../Response';
+
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -14,6 +16,10 @@ export class MomentService {
   private apiUrl = `${this.baseAPIUrl}api/moments`;
 
   constructor(private http: HttpClient) { }
+
+  getMoments(): Observable<Response<Moment[]>> { // Dessa maneira, consigo receber dados diferentes, que não sejam só os dados apenas do Moments
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   // Recebe no parametro os dados no formulário para que possa ser enviado para a API.
   createMoment(formData: FormData): Observable<FormData> {
