08b29154d748b463c9ee8902adbb3fea66552135
Felipe-ABS <FelipeABS2@Gmail.com>
2023-09-28 19:26:50 -0300

    34 - Busca de dados no Angular

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -17,7 +17,8 @@ export class HomeComponent implements OnInit {
   moments: Moment[] = [];
   baseApiUrl = environment.baseAPIUrl;
 
-  // TODO search
+  faSearch = faSearch;
+  searchTerm: string = '';
 
   constructor(private momentService: MomentService) { }
 
@@ -33,6 +34,19 @@ export class HomeComponent implements OnInit {
       this.moments = data;
 
     });
+  }
+
+  search(e: Event): void {
+    // target -> aqui eu estou pegando o que foi escrito dentro do meu input
+    // Como um elemento input do HTML, ja que eu não consigo pegar diretamente do evento
+    // const target = e.target as HTMLInputElement;
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    // Neste caso aqui, deve usar apenas a arrowFunction sem as chaves, para que ele possa retornar o valor
+    this.moments = this.allMoments.filter(moment =>
+      moment.title.toLowerCase().includes(value)
+    )
   }
 
 }
